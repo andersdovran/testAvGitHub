79cb206a6ca47f90f2cc03360c2b1e63bd80034d
andersdovran <anders.dovran@hotmail.com>
2014-03-26 13:48:00 +0100

    skrev noe

--- TestGitHub/src/testgithub/TestGitHub.java
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -16,7 +16,7 @@ public class TestGitHub
 {
     public static void main(String[] args) 
     {
-        JOptionPane.showMessageDialog(null,"Yo" );
+        JOptionPane.showMessageDialog(null,"Yo YO YOO" );
     }
     
 }
