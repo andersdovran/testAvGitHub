26d8b4be090cc09b93f1aa4ca47a45499f9f151b
andersdovran <anders.dovran@hotmail.com>
2014-03-26 13:41:37 +0100

    Laget prosjekt

---
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Kristin
+ */
+public class TestGitHub
+{
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null,"Hei hei hei" );
+    }
+    
+}
